ea5fe706114d96fa37e61c821764ebc4f78faa2b
Ricardo Lima <luizricardodelima9@gmail.com>
2022-01-30 17:04:56 +0100

    Final solution

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('oi');
+const express = require('express');
+const app = express();
+
+// Logger middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('This is an implementation of Request Header Parser Microservice proposed by FreeCodeCamp');
+});
+
+app.get('/api/whoami', function (req, res) {
+    console.log(req);
+    res.json({
+        ipaddress: req.ip,
+        language: req.headers['accept-language'],
+        software: req.headers['user-agent']
+    });
+});
+
+app.listen(3000);
